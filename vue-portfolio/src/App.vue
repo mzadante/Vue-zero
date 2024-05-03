83e282b8a7c9a962cd89f08d0e458174d5e39c7e
Dante Martinez <mzadantemartinez@gmail.com>
2024-05-03 11:18:48 -0300

    start portafolio

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
   <MainCard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import MainCard from './components/MainCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    
     MainCard
   }
 }
