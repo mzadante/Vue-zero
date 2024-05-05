078277105c8c2f6ce74260f10996c423eab6e3bc
Dante Martinez <mzadantemartinez@gmail.com>
2024-05-04 22:17:19 -0300

    tutorial arrays

--- tutorial-vue/src/App.vue
+++ tutorial-vue/src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const name = 'Nicolas con texto dinamico esto se llama interpolacion de texto tambien conocido como bigotes o mustache';
+const arrayFrutas = [
+  {
+    id: 1,
+    name: 'Manzana',
+    color: 'red',
+    price: '$1500'
+  },
+  {
+    id: 2,
+    name: 'Pera',
+    color: 'green',
+    price: '$2000'
+  },
+  {
+    id: 3,
+    name: 'Banana',
+    color: 'yellow',
+    price: '$3000'
+  }
+];
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <ul>
+    <li v-for="fruta in arrayFrutas" :key="fruta.id">{{ fruta.name }}, {{ fruta-price }}</li>
+  </ul> 
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
